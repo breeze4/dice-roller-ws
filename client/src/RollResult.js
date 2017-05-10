2c109a0b1835be81f7a65a306139683c511e1eea
breeze4 <ambiturner0@gmail.com>
2017-05-09 20:57:56 -0700

    simple relative time

--- client/src/RollResult.js
+++ client/src/RollResult.js
@@ -1,12 +1,16 @@
 import React, { PureComponent } from 'react';
+import moment from 'moment';
 
 class RollResult extends PureComponent {
   render() {
     return (
       <div>Roll results
         {this.props.rolls.map((result, index) => {
+          const relativeTime = moment(result.timestamp).fromNow();
           return (
-            <div key={index}>{`d${result.dice}`}: {result.roll}</div>
+            <div key={index}>
+              {`d${result.dice}`}: {result.roll} - <small>{relativeTime}</small>
+              </div>
           )
         })}
       </div>
